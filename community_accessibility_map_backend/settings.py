46135fcd7559eec5c38af4f13c56427e5b313451
shaikahalru <sha101kah@gmail.com>
2025-10-27 04:56:30 +0300

    connect to database

--- community_accessibility_map_backend/settings.py
+++ community_accessibility_map_backend/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'community_accessibility_map_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'community_accessibility_map',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
