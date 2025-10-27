da1f3e1513fdaeefb1cd96ca12d1b2a5e21f7b2f
shaikahalru <sha101kah@gmail.com>
2025-10-27 13:22:03 +0300

    create category post request

--- main_app/views.py
+++ main_app/views.py
@@ -8,10 +8,31 @@ from .serializers import *
 
 # Create your views here.
 
+# ------ Category requests
 class CategoryIndex(APIView):
 
     def get(self, request):
-        queryset = Category.objects.all()
-        serializer = CategorySerializer(queryset, many=True)
+        try:
+            queryset = Category.objects.all()
+            serializer = CategorySerializer(queryset, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+    def post(self, request):
+        try:
+            serializer = CategorySerializer(data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
