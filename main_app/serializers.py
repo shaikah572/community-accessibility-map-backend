95fdd8d019a8606b56f94c6d0302993b5e386889
shaikahalru <sha101kah@gmail.com>
2025-10-28 07:03:06 +0300

    create user, marker, and comment serializers

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,8 +1,39 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth.models import User
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+
+class MarkerSerializer(serializers.ModelSerializer):
+
+    category = serializers.CharField(read_only=True, source='category.name')
+
+    class Meta:
+        model = Marker
+        fields = '__all__'
+        read_only_fields = ['created_by']
+
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    marker = serializers.CharField(read_only=True, source='marker.name')
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ['created_by']
