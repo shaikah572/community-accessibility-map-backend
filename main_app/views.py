024b7093c08c63e1dddb1c916ff6d7b41caaa61f
shaikahalru <sha101kah@gmail.com>
2025-10-29 13:26:30 +0300

    created user detail & delete views

--- main_app/views.py
+++ main_app/views.py
@@ -16,8 +16,54 @@ from django.contrib.auth.models import User
 
 # Create your views here.
 
+# ------ User views
+class UserDetail(APIView):
 
-# ------ Category requests
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        try:
+            serializer = UserSerializer(request.user)
+
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    def put(self, request):
+        try:
+            user = User.objects.get(id= request.user.id)
+            serializer = UserSerializer(user, data=request.data)
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+
+class UserDelete(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def delete(self, request):
+        try:
+            user = User.objects.get(id= request.user.id)
+            user.delete()
+
+            return Response({
+                'message': f'User has been deleted.'},
+                status=status.HTTP_204_NO_CONTENT)
+        
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        
+
+# ------ Category views
 class CategoryIndex(APIView):
 
     def get(self, request):
@@ -87,7 +133,7 @@ class CategoryDetail(APIView):
 # ----------
 
 
-# ------ Marker requests
+# ------ Marker views
 class MarkerIndex(APIView):
 
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -163,7 +209,7 @@ class MarkerDetail(APIView):
 # ----------
 
 
-# ------ Comment requests
+# ------ Comment views
 class CommentIndex(APIView):
 
     permission_classes = [IsAuthenticatedOrReadOnly]
