9a0ca02d71fff5234d9f38a2239175d5567286e8
shaikahalru <sha101kah@gmail.com>
2025-10-29 11:26:44 +0300

    created signup view

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import *
 
 urlpatterns = [
+    path('signup/', SignupView.as_view(), name='signup'),
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('markers/', MarkerIndex.as_view(), name='marker_index'),
--- main_app/views.py
+++ main_app/views.py
@@ -5,14 +5,17 @@ from rest_framework.permissions import (
     IsAuthenticatedOrReadOnly, 
     IsAuthenticated, 
     IsAdminUser,
-    AllowAny )
+    AllowAny, )
 
 from .models import *
 from .serializers import *
 
 from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
+
 
 # Create your views here.
+
 
 # ------ Category requests
 class CategoryIndex(APIView):
@@ -210,14 +213,39 @@ class CommentDelete(APIView):
 # ----------
 
 
+# ------ Signup 
+class SignupView(APIView):
 
+    permission_classes = [AllowAny]
 
+    def post(self, request):
+        try:
+            username = request.data.get('username')
+            email = request.data.get('email')
+            password = request.data.get('password')
 
+            if not username or not email or not password:
+                return Response(
+                    {'error': 'Please provide a username, password, and email'},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+            
+            if User.objects.filter(username=username).exists():
+                return Response(
+                    {'error': 'User already exists'},
+                    status=status.HTTP_400_BAD_REQUEST 
+                )
+            
+            user = User.objects.create_user(
+                username=username, email=email, password=password
+            )
 
-
+            return Response(
+                {"id": user.id, "username": user.username, "email": user.email},
+                status=status.HTTP_201_CREATED,
+            )
         
-    
+        except Exception as error:
+            return Response({'error': str(error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-        
-          
         
